24f4f995d604bdca096b8760adde4b9c5822766d
Raí Siqueira <rai93siqueira@gmail.com>
2019-02-11 21:47:45 -0300

    some updates
    Signed-off-by: Raí Siqueira <rai93siqueira@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaskDirectiveModule } from '@justa/mask-directive';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, MaskDirectiveModule],
+  imports: [BrowserModule, ReactiveFormsModule, FormsModule, MaskDirectiveModule],
   providers: [],
   bootstrap: [AppComponent],
 })
