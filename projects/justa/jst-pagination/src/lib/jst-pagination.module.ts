9202874222ea25f4a6e928672d0ec7141b977044
Matheus Gome <matheus.gomes@justa.com.vc>
2019-04-05 17:59:48 -0300

    New library added to handle list pagination

---
+++ projects/justa/jst-pagination/src/lib/jst-pagination.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { JstPaginationComponent } from './jst-pagination/jst-pagination.component';
+
+@NgModule({
+  declarations: [JstPaginationComponent],
+  imports: [],
+  exports: [JstPaginationComponent],
+})
+export class JstPaginationModule {}
---
+++ projects/justa/jst-pagination/src/lib/jst-pagination/jst-pagination.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jst-pagination',
+  templateUrl: './jst-pagination.component.html',
+  styleUrls: ['./jst-pagination.component.scss'],
+})
+export class JstPaginationComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
