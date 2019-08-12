3d51bd5957b32b69a5dd32ca08c5adff79dc54aa
Raí Siqueira <rai93siqueira@gmail.com>
2019-08-12 13:49:38 -0300

    Fix npm links
    Signed-off-by: Raí Siqueira <rai93siqueira@gmail.com>

--- projects/justa/validators/karma.conf.js
+++ projects/justa/validators/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../../coverage/justa/ng-validators'),
+      dir: require('path').join(__dirname, '../../../coverage/justa/validators'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
